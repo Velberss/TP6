b810c86354e5be6deb6d2eb84c9b079e97b0c75a
Velber <goncalvesvelber@gmail.com>
2024-09-18 23:47:51 -0300

    1°, 2° e 3° feito

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,21 @@
 public class Main {
     public static void main(String[] args) {
         ArvoreBinaria arvore = new ArvoreBinaria();
-        No c1 = new No(1);
-        No c2 = new No(2);
-        No c3 = new No(3);
-        No c4 = new No(4);
-        No c5 = new No(5);
-        No c6 = new No(6);
-        No c7 = new No(7);
-        No c8 = new No(8);
-        No c9 = new No(9);
-        No c10 = new No(10);
+        No c1 = new No(10);
+        No c2 = new No(5);
+        No c3 = new No(20);
+        No c4 = new No(3);
+        No c5 = new No(7);
+        No c6 = new No(30);
+    
         
         arvore.inserir(c1);
         arvore.inserir(c2);
         arvore.inserir(c3);
         arvore.inserir(c4);
         arvore.inserir(c5);
-        arvore.inserir(c6);  
-        arvore.inserir(c7);
-        arvore.inserir(c8);
-        arvore.inserir(c9);  
-        arvore.inserir(c10);    
+        arvore.inserir(c6);
+         
 
         int a = arvore.contarNosFolhas(c1);
         System.out.println(a);
