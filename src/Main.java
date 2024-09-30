6ef75444f9e51f24a08c3b4d06e34bffdfbdb265
Velber <goncalvesvelber@gmail.com>
2024-09-30 16:38:44 -0300

    feito

--- src/Main.java
+++ src/Main.java
@@ -6,17 +6,12 @@ public class Main {
         No c2 = new No(2);
         No c3 = new No(11);
         No c4 = new No(1);
-        No c5 = new No(3);
-        No c6 = new No(12);
+
         
+        arvore.inserir(c1);
+        arvore.inserir(c2);
+        arvore.inserir(c3);
         arvore.inserir(c4);
-        arvore.inserir(c5);
-        arvore.inserir(c6);
-        arvore.preOrdem(c4);
-        
-        pilha.empilhar(c1);
-        pilha.empilhar(c2);
-        pilha.empilhar(c3);
-        pilha.posOrdem(c1);
+        pilha.emOrdem(c1);
     }
 }
--- src/Pilha.java
+++ src/Pilha.java
@@ -1,23 +1,22 @@
 public class Pilha {
     private No topo;
-    
-    public Pilha( ) {
+
+    public Pilha() {
         this.topo = null;
     }
-        
-    public boolean vazia( ){
+
+    public boolean vazia() {
         return (topo == null);
     }
 
-    public void empilhar(No no){
+    public void empilhar(No no) {
         no.prox = topo;
         topo = no;
     }
 
-    public No desempilhar(){
-        if(vazia( )){
-            System.out.println("Pilha vazia!");
-            return null;
+    public No desempilhar() {
+        if (vazia()) {
+            throw new RuntimeException("Pilha vazia!");
         } else {
             No aux = topo;
             topo = topo.prox;
@@ -26,9 +25,10 @@ public class Pilha {
         }
     }
 
-    public void preOrdem(No no) {
-        if (no == null)
+    public void emOrdem(No no) {
+        if (no == null) {
             return;
+        }
 
         Pilha pilha = new Pilha();
         No aux = no;
@@ -36,12 +36,32 @@ public class Pilha {
         while (aux != null || !pilha.vazia()) {
             while (aux != null) {
                 pilha.empilhar(aux);
-                System.out.print(aux.valor + " ");
                 aux = aux.esquerda;
             }
+
             aux = pilha.desempilhar();
-            if (aux != null) {
-                aux = aux.direita;
+            System.out.print(aux.valor + " ");
+            aux = aux.direita;
+        }
+    }
+
+    public void preOrdem(No no) {
+        if (no == null) {
+            return;
+        }
+
+        Pilha pilha = new Pilha();
+        pilha.empilhar(no);
+
+        while (!pilha.vazia()) {
+            No aux = pilha.desempilhar();
+            System.out.print(aux.valor + " ");
+
+            if (aux.direita != null) {
+                pilha.empilhar(aux.direita);
+            }
+            if (aux.esquerda != null) {
+                pilha.empilhar(aux.esquerda);
             }
         }
     }
@@ -50,28 +70,25 @@ public class Pilha {
         if (no == null) {
             return;
         }
+
         Pilha pilha1 = new Pilha();
         Pilha pilha2 = new Pilha();
-
         pilha1.empilhar(no);
 
         while (!pilha1.vazia()) {
             No aux = pilha1.desempilhar();
-
-            if (aux != null) {
-                pilha2.empilhar(aux);
-                if (aux.esquerda != null)
-                    pilha1.empilhar(aux.esquerda);
-                if (aux.direita != null)
-                    pilha1.empilhar(aux.direita);
+            pilha2.empilhar(aux); 
+            if (aux.esquerda != null) {
+                pilha1.empilhar(aux.esquerda);
+            }
+            if (aux.direita != null) {
+                pilha1.empilhar(aux.direita);
             }
         }
-
         while (!pilha2.vazia()) {
-            No aux = pilha2.desempilhar(); 
-            if (aux != null) {
-                System.out.print(aux.valor + " ");
-            }
+            No aux = pilha2.desempilhar();
+            System.out.print(aux.valor + " ");
         }
     }
+
 }
